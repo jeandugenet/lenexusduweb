225da53de533997a687d1dfa46da37de392a5a29
jeandugenet <150827555+jeandugenet@users.noreply.github.com>
2026-01-01 21:33:54 +0100

    mise à jour vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'public/build',
+    manifest: true,
+    rollupOptions: {
+      output: {
+        // rien de particulier
+      }
+    },
+  },
+  base: '/', // important
 });
-
